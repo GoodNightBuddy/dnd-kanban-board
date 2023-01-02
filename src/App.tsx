f39af26974374f8a16bc5cec6c94c2a0aa028296
GoodNightBuddy <fun.love.nuts@gmail.com>
2023-01-02 12:56:29 +0200

    Added darkMode button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import Column from "./components/Column";
+import DarkModeIconButton from "./components/DarkModeIconButton";
 import { ColumnType } from "./utils/enum";
 
 export const App = () => (
@@ -16,7 +17,7 @@ export const App = () => (
     >
       Welcome to DnD Kanban
     </Heading>
-
+    <DarkModeIconButton position={'absolute'} top={0} right={2}/>
     <Container maxW={'container.lg'} px={4} py={10}>
       <DndProvider backend={HTML5Backend}>
         <SimpleGrid columns={{ base: 1, md: 4 }} spacing={{ base: 16, md: 4 }}>
---
+++ src/components/DarkModeIconButton.tsx
@@ -0,0 +1,21 @@
+import { IconButton, useColorMode } from '@chakra-ui/react';
+import React from 'react';
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+const DarkModeIconButton = ({...rest}: React.ComponentPropsWithoutRef<typeof IconButton>) => {
+  
+  const {colorMode, toggleColorMode} = useColorMode();
+
+  const isDark = colorMode === 'dark';
+
+  return (
+    <IconButton
+      onClick={toggleColorMode}
+      icon={isDark ? <FaMoon /> : <FaSun />}
+      aria-label={'dark-mode-toggle'}
+      {...rest}
+      />
+  );
+};
+
+export default DarkModeIconButton;
